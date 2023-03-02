1a82478d0351205d31d5cb0e826f55c699cb7d5e
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-02 12:32:43 +0530

    Added : Calculating Length of line using co-ordinates

--- src/com/birdgelabz_linecomparison/LineComparison.java
+++ src/com/birdgelabz_linecomparison/LineComparison.java
@@ -1,7 +1,27 @@
 package com.birdgelabz_linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args) {
+    void lengthOfLine() {
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x1 Co-ordinate = ");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter x2 Co-ordinate = ");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y1 Co-ordinate = ");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter y2 Co-ordinate = ");
+        double y2 = sc.nextDouble();
+
+        double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("Length of a Line = " + length1);
+    }
+    public static void main(String[] args) {
+        LineComparison runner = new LineComparison();
+        runner.lengthOfLine();
     }
 }
